c3e0771dda81b8bdfe0541e76a27b83fc10ea2de
Alan Rodas Bonjour <alanrodas@gmail.com>
2024-11-16 08:30:35 -0300

    chore: Adding support for docker and docker compose

--- src/api/ApiCancha.ts
+++ src/api/ApiCancha.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiCancha = axios.create({
-    baseURL:'http://localhost:8080/cancha',
+    baseURL:window.location.protocol + "//" + window.location.host + '/api',
     withCredentials:true,
 })
 
