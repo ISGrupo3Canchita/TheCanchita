be566aedfbaa6700a0121933f91cd70bf2e5faff
Gonza1101 <gonzalo.villalba1987@gmail.com>
2024-11-30 20:35:14 -0300

    seccion para ver usuarios y modificar Rol

--- src/Components/Usuario/UsuarioLista.tsx
+++ src/Components/Usuario/UsuarioLista.tsx
@@ -12,15 +12,17 @@ export const UsuarioLista: React.FC<listSUsuarioProps> = ({usuarios, cambioRol})
         <>
             <div className="container mt-3" >
                 <div className="row">
-                    <div className="col-sm-2 text-center bg-light">
-                        <h4>Usuarios </h4>
+                    <div className="col-2 text-center bg-light">
+                        <p className="h5">Lista Usuarios</p>
+                        <p>“Tengo dos problemas para jugar al fútbol. <br/>Uno es la pierna izquierda. <br/>El otro es la pierna derecha”</p>
+                        <p className="h6">Roberto Fontanarrosa</p>
                     </div>
 
-                    <div className="col-sm-10 bg-light">
+                    <div className="col-10 bg-light">
                         <div className="container row">
                             {
                                 usuarios.map((usuario)=>(
-                                    <UsuarioRow key={usuario.id} usuario={usuario} cambioRol={cambioRol}/>
+                                    <UsuarioRow key={usuario.id} usuario={usuario} cambioRol={cambioRol}/> 
                                 ))
                             }
                         </div>
--- src/Components/Usuario/UsuarioRow.tsx
+++ src/Components/Usuario/UsuarioRow.tsx
@@ -13,28 +13,37 @@ export const UsuarioRow : React.FC<UsuarioRowProps> = ({cambioRol, usuario})=>{
         // console.log(inputRolRef.current!.value)
         cambioRol(usuario.id,inputRolRef.current!.value)
     }
+
     return (
         <> 
-            <div className="container col-3 text-center" >
+            <div className="container-fluid col-3 text-center" >
                 <div className="card" style={{width:200, backgroundColor:"#C7C7C7 "}} >
-                    <img className="card-img-top" src="/Images/Potrero_1.jpg"  alt="Imagen De Cancha"></img>
+                    <img 
+                        key={usuario.id}
+                        className="card-img-top" 
+                        src={`https://robohash.org/${usuario.id}.png`}
+                        // "https://rickandmortyapi.com/api/character/avatar/49.jpeg"
+                        alt={usuario.nombre}></img>
                     <div className="card-body">
-                        <h4 className="card-title">{usuario.nombre}</h4>
-                        <p className="card-text">Email: {usuario.email}</p>
-                        <p className="card-text">Telefono: {usuario.telefono}</p>
-                        <h4 className="card-text" style={{color:"#2986cc"}}>{usuario.rol}</h4>
-                        <form 
-                           
-                        >
-                            <select
+                        <div>
+                            <p className="h5 card-title">{usuario.nombre}</p>
+                            <p className="card-text">Email: {usuario.email}</p>
+                            <p className="card-text">Telefono: {usuario.telefono}</p>
+                            <p className="h5 card-text" style={{color:"#2986cc"}}>{usuario.rol}</p>
+                        </div>
+                        
+                        <div className="mb-1 mt-1">
+                            <form>
+                                <select
                                 ref={inputRolRef}
-                             className="form-select form-select-sm mt-3">
-                                <option>Administrador</option>
-                                <option>Eliminado</option>
-                                <option>Operador</option>
-                                <option>Usuario</option>
-                            </select>
-                        </form> 
+                                className="form-select form-select-sm-2">
+                                    <option>Administrador</option>
+                                    <option>Eliminado</option>
+                                    <option>Operador</option>
+                                    <option>Usuario</option>
+                                </select>
+                            </form> 
+                        </div>
                 
                         <div className="btn-group btn-group-sm">
                             <button className="btn btn-success" type="button" onClick={handleCambiarRol}>Asignar</button>
