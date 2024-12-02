3fab76a071572bf020af23ef85e2b03b27538c6b
Ramiro Varitis <ramyvaritis@gmail.com>
2024-12-02 19:06:14 -0300

    arreglado error en crearCancha, ahora si carga el estado

--- src/Components/Cancha/CanchaCreate.tsx
+++ src/Components/Cancha/CanchaCreate.tsx
@@ -17,11 +17,13 @@ export const CanchaCreate : React.FC<{token:string}> = ( { token } ) => {
     const inputDireccionRef = useRef<HTMLInputElement>(null);
     const inputHorarioInicio = useRef<HTMLInputElement>(null);
     const inputHorarioFin = useRef<HTMLInputElement>(null);
-    const [estado, setEstado] = useState<string>("");
-
+    const [estado, setEstado] = useState<string>("Habilitada");
 
 
     const handlerBotonCrear = async () => {
+        if (!(inputNombreRef && inputDireccionRef && inputHorarioInicio && inputHorarioFin )) {
+            alert("Complete todos los campos");
+        }
         const cancha: Cancha = {
             id              : "",
             nombreCancha    : inputNombreRef.current!.value,
@@ -40,13 +42,9 @@ export const CanchaCreate : React.FC<{token:string}> = ( { token } ) => {
 
     const handleKeyUp= (e : React.KeyboardEvent) => {
         if (e.key === "Enter") {
-            if (inputNombreRef.current!.value && inputDireccionRef.current!.value && inputHorarioInicio.current!.value &&
-                inputHorarioFin.current!.value && estado){
-                handlerBotonCrear();
-            }
-            else{
-                alert('Complete todos los campos')
-            }
+            handlerBotonCrear();
+            
+           
         }
     }
 
@@ -54,72 +52,77 @@ export const CanchaCreate : React.FC<{token:string}> = ( { token } ) => {
         navigate('/admin')
     }
 
-    return (
+   return (
         <>
-        {/* <BarraNavegacion cambio={handleVolver} key={usuario.id} nombre={usuario.nombre} rol={usuario.rol} /> */}
-        <div className="container pt-5 my-5 text-center border">
+          {/* <BarraNavegacion cambio={handleVolver} key={usuario.id} nombre={usuario.nombre} rol={usuario.rol} /> */}
+          <div className="container pt-5 my-5 text-center border">
             <h2>Crear Cancha</h2>
             <form>
-                <div className="mb-3 mt-3">
-                    <label className="form-label"> Nombre de la Cancha
-                        <input type="text"
-                        className="form-control"
-                        ref={inputNombreRef}
-                        placeholder="Nombre"
-                        onKeyUp={handleKeyUp}
-                          />
-                    </label>
-                </div>
-                <div className="mb-3 mt-3">
-                    <label className="form-label"> Direccion
-                        <input type="text" 
-                        className="form-control"
-                        ref={inputDireccionRef}
-                        placeholder="Calle Falsa 123"
-                        onKeyUp={handleKeyUp}
-                        />
-                    </label>
-                </div>
-                <div className="mb-3 mt-3">
-                    <label className="form-group"> Horario Inicio
-                        <input type="time" 
-                        className="form-control"
-                        ref={inputHorarioInicio}
-                        onKeyUp={handleKeyUp}
-                        />
-                    </label>
-                </div>
-                <div className="mb-3 mt-3">
-                    <label className="form-group"> Horario Cierre
-                        <input type="time" 
-                        className="form-control"
-                        ref={inputHorarioFin}
-                        onKeyUp={handleKeyUp}
-                        />
-                    </label>
-                </div>
-                <div className="mb-3 mt-3">
-            <label className="form-label">
-              Estado
-              <select
-                className="form-control"
-                value={estado}
-                onChange={(e) => setEstado(e.target.value)}
+              <div className="mb-3 mt-3">
+                <label className="form-label">
+                  Nombre de la Cancha
+                  <input
+                    type="text"
+                    className="form-control"
+                    ref={inputNombreRef}
+                    placeholder="Nombre"
+                    onKeyUp={handleKeyUp}
+                  />
+                </label>
+              </div>
+              <div className="mb-3 mt-3">
+                <label className="form-label">
+                  Dirección
+                  <input
+                    type="text"
+                    className="form-control"
+                    ref={inputDireccionRef}
+                    placeholder="Calle Falsa 123"
+                    onKeyUp={handleKeyUp}
+                  />
+                </label>
+              </div>
+              <div className="mb-3 mt-3">
+                        <label className="form-group"> Horario Inicio
+                            <input type="time" 
+                            className="form-control"
+                            ref={inputHorarioInicio}
+                            onKeyUp={handleKeyUp}
+                            />
+                        </label>
+                    </div>
+                    <div className="mb-3 mt-3">
+                        <label className="form-group"> Horario Cierre
+                            <input type="time" 
+                            className="form-control"
+                            ref={inputHorarioFin}
+                            onKeyUp={handleKeyUp}
+                            />
+                        </label>
+                    </div>
+              <div className="mb-3 mt-3">
+                <label className="form-label">
+                  Estado
+                  <select
+                    className="form-control"
+                    value={estado}
+                    onChange={(e) => setEstado(e.target.value)}
+                  >
+                    <option value="Habilitada">Habilitada</option>
+                    <option value="Deshabilitada">Deshabilitada</option>
+                  </select>
+                </label>
+              </div>
+              <button
+                type="button"
+                className="btn btn-dark"
+                onClick={handlerBotonCrear}
               >
-                <option value="Habilitada">Habilitada</option>
-                <option value="Deshabilitada">Deshabilitada</option>
-              </select>
-            </label>
-          </div>
-                <button
-                            type="button" 
-                            className="btn btn-dark"
-                            onClick={handlerBotonCrear}>
-                            Crear Cancha
-                </button>
+                Crear Cancha
+              </button>
             </form>
             {mensaje && <p className="mt-3">{mensaje}</p>}
-        </div>
+          </div>
         </>
-    )
+      );
 };
